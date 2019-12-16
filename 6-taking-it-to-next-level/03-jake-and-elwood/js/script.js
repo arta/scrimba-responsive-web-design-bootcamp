460d17d8467edde13b57f9ed500b5f047cbfe7ec
Martin Fencl <martin.fencl@gmail.com>
2019-12-16 17:22:51 -0600

    write header nav open js handler
    Basically, adds our open state class.

--- 6-taking-it-to-next-level/03-jake-and-elwood/js/script.js
+++ 6-taking-it-to-next-level/03-jake-and-elwood/js/script.js
@@ -1,6 +1,11 @@
 const btnHeaderNavClose = document.querySelector('.btn-header-nav-close');
+const btnHeaderNavOpen = document.querySelector('.btn-header-nav-open');
 const headerNav = document.querySelector('.header-nav');
 
 btnHeaderNavClose.addEventListener('click', () => {
     headerNav.classList.remove('header-nav-open');
-})
+});
+
+btnHeaderNavOpen.addEventListener('click', () => {
+    headerNav.classList.add('header-nav-open');
+});
