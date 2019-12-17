725ada5bd8dd5f1a12405effe16f2ecd1ed85378
Martin Fencl <martin.fencl@gmail.com>
2019-12-17 11:52:58 -0600

    use double dash `--` in modifier classes names
    Old practice I recall, makes sense now, may improve semantics, readability; we'll see.

--- 6-taking-it-to-next-level/03-jake-and-elwood/js/script.js
+++ 6-taking-it-to-next-level/03-jake-and-elwood/js/script.js
@@ -1,11 +1,11 @@
-const btnHeaderNavClose = document.querySelector('.btn-header-nav-close');
-const btnHeaderNavOpen = document.querySelector('.btn-header-nav-open');
+const btnHeaderNavClose = document.querySelector('.btn--header-nav-close');
+const btnHeaderNavOpen = document.querySelector('.btn--header-nav-open');
 const headerNav = document.querySelector('.header-nav');
 
 btnHeaderNavClose.addEventListener('click', () => {
-    headerNav.classList.remove('header-nav-open');
+    headerNav.classList.remove('header-nav--open');
 });
 
 btnHeaderNavOpen.addEventListener('click', () => {
-    headerNav.classList.add('header-nav-open');
+    headerNav.classList.add('header-nav--open');
 });
